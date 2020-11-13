3e8efbc5c0e9314a30c3fd316299b195b465bc01
BM-K <klbm126@naver.com>
2020-11-13 17:40:47 +0900

    Update training_sts.py

--- training_sts.py
+++ training_sts.py
@@ -1,11 +1,3 @@
-"""
-This examples trains BERT (or any other transformer model like RoBERTa, DistilBERT etc.) for the STSbenchmark from scratch. It generates sentence embeddings
-that can be compared using cosine-similarity to measure the similarity.
-Usage:
-python training_nli.py
-OR
-python training_nli.py pretrained_transformer_model_name
-"""
 from torch.utils.data import DataLoader
 import math
 from sentence_transformers import SentenceTransformer,  SentencesDataset, LoggingHandler, losses, models, util
@@ -18,23 +10,17 @@ import os
 import gzip
 import csv
 
-#### Just some code to print debug information to stdout
 logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO,
                     handlers=[LoggingHandler()])
-#### /print debug information to stdout
 
-
-#You can specify any huggingface/transformers pre-trained model here, for example, bert-base-uncased, roberta-base, xlm-roberta-base
 model_name = 'ETRI_KoBERT/003_bert_eojeol_pytorch'
 
-# Read the dataset
 train_batch_size = 16
 num_epochs = 4
 model_save_path = 'output/training_stsbenchmark_'+model_name.replace("/", "-")+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-# Use Huggingface/transformers model (like BERT, RoBERTa, XLNet, XLM-R) for mapping tokens to embeddings
 word_embedding_model = models.Transformer(model_name, isKor=True)
 
 # Apply mean pooling to get one fixed sized sentence vector
@@ -45,7 +31,6 @@ pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension
 
 model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
-# Convert the dataset to a DataLoader ready for training
 logging.info("Read STSbenchmark train dataset")
 
 train_samples = []
@@ -98,21 +83,11 @@ model.fit(train_objectives=[(train_dataloader, train_loss)],
           warmup_steps=warmup_steps,
           output_path=model_save_path)
 
-
 ##############################################################################
 #
 # Load the stored model and evaluate its performance on STS benchmark dataset
 #
 ##############################################################################
-
-#word_embedding_model = models.Transformer(model_save_path)
-
-#pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(),
-#        pooling_mode_mean_tokens=True,
-#        pooling_mode_cls_token=False,
-#        pooling_mode_max_tokens=False)
-
-#model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
 model = SentenceTransformer(model_save_path)
 test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(test_samples, name='sts-test')
