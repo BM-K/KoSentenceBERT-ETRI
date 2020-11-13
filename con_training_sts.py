057cb7db717084d2042f8fcbb9e1b759552ae419
BM-K <klbm126@naver.com>
2020-11-13 17:41:15 +0900

    Update con_training_sts.py

--- con_training_sts.py
+++ con_training_sts.py
@@ -1,9 +1,3 @@
-"""
-This example loads the pre-trained SentenceTransformer model 'bert-base-nli-mean-tokens' from the server.
-It then fine-tunes this model for some epochs on the STS benchmark dataset.
-Note: In this example, you must specify a SentenceTransformer model.
-If you want to fine-tune a huggingface/transformers model like bert-base-uncased, see training_nli.py and training_stsbenchmark.py
-"""
 from torch.utils.data import DataLoader
 import math
 from sentence_transformers import SentenceTransformer,  SentencesDataset, LoggingHandler, losses, util, InputExample
@@ -14,25 +8,18 @@ import os
 import gzip
 import csv
 
-#### Just some code to print debug information to stdout
 logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO,
                     handlers=[LoggingHandler()])
-#### /print debug information to stdout
 
-
-# Read the dataset
 model_name = './output/training_nli_ETRI_KoBERT-003_bert_eojeol' #'bert-base-nli-mean-tokens'
 train_batch_size = 16
 num_epochs = 4
 model_save_path = 'output/training_stsbenchmark_continue_training-'+model_name+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-
-# Load a pre-trained sentence transformer model
 model = SentenceTransformer(model_name)
 
-# Convert the dataset to a DataLoader ready for training
 logging.info("Read STSbenchmark train dataset")
 
 train_samples = []
@@ -71,8 +58,6 @@ train_loss = losses.CosineSimilarityLoss(model=model)
 logging.info("Read STSbenchmark dev dataset")
 evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, name='sts-dev')
 
-
-# Configure the training. We skip evaluation in this example
 warmup_steps = math.ceil(len(train_dataset) * num_epochs / train_batch_size * 0.1) #10% of train data for warm-up
 logging.info("Warmup-steps: {}".format(warmup_steps))
 
