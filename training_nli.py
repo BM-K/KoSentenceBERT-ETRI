b8aba4825d96936cbb9ccc28ca5ca0409e33c8fb
BM-K <klbm126@naver.com>
2020-11-13 17:40:05 +0900

    Update training_nli.py

--- training_nli.py
+++ training_nli.py
@@ -1,12 +1,3 @@
-"""
-The system trains BERT (or any other transformer model like RoBERTa, DistilBERT etc.) on the SNLI + MultiNLI (AllNLI) dataset
-with softmax loss function. At every 1000 training steps, the model is evaluated on the
-STS benchmark dataset
-Usage:
-python training_nli.py
-OR
-python training_nli.py pretrained_transformer_model_name
-"""
 from torch.utils.data import DataLoader
 import math
 from sentence_transformers import models, losses
@@ -20,16 +11,12 @@ import gzip
 import csv
 
 
-#You can specify any huggingface/transformers pre-trained model here, for example, bert-base-uncased, roberta-base, xlm-roberta-base
-model_name = "./ETRI_KoBERT/003_bert_eojeol_pytorch"#sys.argv[1] if len(sys.argv) > 1 else 'bert-base-uncased'
-# Read the dataset
-train_batch_size = 16
+model_name = "./ETRI_KoBERT/003_bert_eojeol_pytorch"
 
+train_batch_size = 16
 
 model_save_path = 'output/training_nli_'+model_name.replace("/", "-")+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-
-# Use Huggingface/transformers model (like BERT, RoBERTa, XLNet, XLM-R) for mapping tokens to embeddings
 word_embedding_model = models.Transformer(model_name, isKor=True)
 
 # Apply mean pooling to get one fixed sized sentence vector
@@ -40,8 +27,6 @@ pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension
 
 model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
-
-# Read the AllNLI.tsv.gz file and create the training dataset
 logging.info("Read AllNLI train dataset")
 
 label2int = {"contradiction": 0, "entailment": 1, "neutral": 2}
@@ -73,13 +58,10 @@ with open('./KorNLUDatasets/KorSTS/tune_dev.tsv', 'rt', encoding='utf-8') as fIn
 
 dev_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, batch_size=train_batch_size, name='sts-dev')
 
-# Configure the training
 num_epochs = 1
 
 warmup_steps = math.ceil(len(train_dataset) * num_epochs / train_batch_size * 0.1) #10% of train data for warm-up
 logging.info("Warmup-steps: {}".format(warmup_steps))
-
-
 
 # Train the model
 model.fit(train_objectives=[(train_dataloader, train_loss)],
